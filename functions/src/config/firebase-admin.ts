654871482ab660154e18cc7c7b289739e525be65
Marco Mondini <10319061+mondial7@users.noreply.github.com>
2026-03-09 22:02:26 +0100

    ✨ | project definition & restart

--- functions/src/config/firebase-admin.ts
+++
@@ -1,8 +0,0 @@
-import * as admin from 'firebase-admin';
-
-// Initialize Firebase Admin
-admin.initializeApp();
-
-export const db = admin.firestore();
-export const auth = admin.auth();
-export { admin };
